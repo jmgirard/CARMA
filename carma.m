6f4503a1db5c73898e5c985768dafb9c40779a0d
Jeffrey Girard <jmg174@pitt.edu>
2014-05-31 22:25:59 -0400

    changes for v4.00

--- carma.m
+++ carma.m
@@ -38,8 +38,8 @@ function carma_OpeningFcn(hObject, ~, handles, varargin)
         Settings.axis_color1 = [1,0,0];
         Settings.axis_color2 = [1,1,0];
         Settings.axis_color3 = [0,1,0];
-        Settings.axis_min = '1';
-        Settings.axis_max = '9';
+        Settings.axis_min = '-100';
+        Settings.axis_max = '100';
         Settings.axis_steps = '9';
         save('default.mat','Settings');
         save('settings.mat','Settings');
@@ -61,6 +61,7 @@ function open_file_Callback(hObject, ~, handles)
     if video_name==0, return; end %Cancel function if no file is selected
     try
         handles.wmp.URL = fullfile(video_path,video_name); %Open multimedia file in WMP window
+        %TODO: Replace mmfileinfo() with wmp.currentMedia.duration
         info = mmfileinfo(fullfile(video_path,video_name)); %Determine file duration
         handles.dur = info.Duration; %Save file duration to GUI data
     catch err
@@ -72,7 +73,8 @@ function open_file_Callback(hObject, ~, handles)
     set(handles.filename,'string',video_name); %Update Video Name window
     set(handles.duration,'string',datestr(info.Duration/24/3600,'HH:MM:SS.FFF')); %Update Duration window
     set(handles.playpause,'enable','on'); %Enable play button to be used
-    handles.rating = zeros(ceil(info.Duration),1); %Initialize rating vector
+%     handles.rating = zeros(ceil(info.Duration),2); %Initialize rating vector
+    handles.rating = [];
     guidata(hObject, handles); %Update GUI data
 
 function settings_Callback(hObject, ~, handles)
@@ -86,7 +88,7 @@ function settings_Callback(hObject, ~, handles)
 function about_Callback(hObject, ~, handles)
 %ABOUT_CALLBACK Runs when selecting About menu option
     line1 = 'Continuous Affect Rating and Media Annotation';
-    line2 = 'Version 3.01 <05-31-2014>';
+    line2 = 'Version 4.00 <05-31-2014>';
     line3 = 'Manual: http://carma.codeplex.com/documentation';
     line4 = 'Support: http://carma.codeplex.com/discussion';
     line5 = 'License: http://carma.codeplex.com/license';
@@ -98,21 +100,17 @@ function playpause_Callback(hObject, ~, handles)
     set(handles.open_file,'enable','off'); %Disable the Open File menu option
     set(handles.settings,'enable','off'); %Disable the Settings menu option
     set(handles.about,'enable','off'); %Disable the About menu option
-    set(handles.report,'string','Get Ready'); %Update Report window
-    set(hObject,'string','3'); pause(1); %Countdown on Play button
-    set(hObject,'string','2'); pause(1); %Countdown on Play button
-    set(hObject,'string','1'); pause(1); %Countdown on Play button
+    set(handles.report,'string','...3...'); pause(1);
+    set(handles.report,'string','..2..'); pause(1);
+    set(handles.report,'string','.1.'); pause(1);
     set(hObject,'string','...'); %Update Play button
-    for i = 1:ceil(handles.dur)
-        temp = []; %Preallocate temporary ratings vector
-        handles.wmp.controls.play(); %Send play command to WMP ActiveX Controller
-        pause(0.05); %Pause to make sure video begins playing before starting ratings
-        timer = tic; %Start timer
-        while toc(timer) < 1.0
-            temp = [temp; get(handles.slider,'value')]; %Collect ratings until timer hits 1 sec
-        end
-        handles.rating(i) = mean(temp); %Add mean rating for that second to ratings vector
-        set(handles.report,'string',datestr(i/24/3600,'HH:MM:SS.FFF')); %Show elapsed time in seconds
+    handles.wmp.controls.play(); %Send play command to WMP
+    while ~strcmp(handles.wmp.playState,'wmppsPlaying'), pause(0.01); end %Wait for WMP to start playing
+    while handles.wmp.controls.currentPosition < handles.wmp.currentMedia.duration
+        pause(0.1); %Wait 1/10 of a second
+        handles.rating = [handles.rating; handles.wmp.controls.currentPosition,get(handles.slider,'value')]; %Add mean rating for that second to ratings vector
+        set(handles.report,'string',datestr(handles.wmp.controls.currentPosition/24/3600,'HH:MM:SS.FFF')); %Show elapsed time in seconds
+        drawnow;
     end
     handles.wmp.controls.pause(); %Send pause command to WMP ActiveX Controller
     set(handles.report,'string','Processing...'); %Update Report window
@@ -122,18 +120,28 @@ function playpause_Callback(hObject, ~, handles)
 function save_rating(handles)
 %SAVE_RATING Runs at the completion of rating
     [~,defaultname,~] = fileparts(get(handles.filename,'string')); %Get multimedia file name without path or extensions
-    [filename,pathname] = uiputfile({'*.xls; *.xlsx','Excel Spreadsheets (*.xls, *.xlsx)';'*.csv','Comma-Separated Values (*.csv)'},'Save as',defaultname); %Prompt user for save path
+    [filename,pathname] = uiputfile({'*.xls; *.xlsx','Excel Spreadsheets (*.xls, *.xlsx)';...
+        '*.csv','Comma-Separated Values (*.csv)'},'Save as',defaultname); %Prompt user for export filepath
     if ~isequal(filename,0) && ~isequal(pathname,0)
         rating = handles.rating; %Get ratings vector
+        maxsec = ceil(max(rating(:,1)));
+        mean_ratings = zeros(maxsec,2);
+        for i = 1:maxsec %Average ratings by second
+            index = rating(:,1)>=i-1 & rating(:,1)<i;
+            mean_ratings(i,:) = [i,mean(rating(index,2))];
+        end
         Settings = importdata('settings.mat');
-        % Add metadata to ratings vector
+        %Add metadata to average ratings
         output = [...
-            {'Filename:',get(handles.filename,'string')};...
-            {'Axis Labels:',sprintf('"%s" to "%s"',get(handles.axis_lower,'string'),get(handles.axis_upper,'string'))};...
-            {'Axis Range:',sprintf('%d to %d',get(handles.slider,'min'),get(handles.slider,'max'))};...
-            {'Number of Axis Steps:',Settings.axis_steps};...
-            {'Second','Rating'};...
-            num2cell(1:length(rating))',num2cell(rating)];
+            {'Filename','Lower Label','Upper Label','Minimum Value','Maximum Value','Number of Steps','Second','Rating'};...
+            cellstr(repmat(get(handles.filename,'string'),size(mean_ratings,1),1)),...
+            cellstr(repmat(get(handles.axis_lower,'string'),size(mean_ratings,1),1)),...
+            cellstr(repmat(get(handles.axis_upper,'string'),size(mean_ratings,1),1)),...
+            num2cell(repmat(get(handles.slider,'Min'),size(mean_ratings,1),1)),...
+            num2cell(repmat(get(handles.slider,'Max'),size(mean_ratings,1),1)),...            
+            num2cell(repmat(Settings.axis_steps,size(mean_ratings,1),1)),...
+            num2cell(mean_ratings)];
+        %Create export file depending on selecting file type
         [~,~,ext] = fileparts(filename);
         if strcmpi(ext,'.XLS') || strcmpi(ext,'.XLSX')
             [success,message] = xlswrite(fullfile(pathname,filename),output); %Write output to Excel spreadsheet
@@ -146,7 +154,8 @@ function save_rating(handles)
             success = cell2csv(fullfile(pathname,filename),output); %Write output to CSV
         end
     else
-        choice = questdlg('You are about to close without saving.','CARMA','Close','Save','Save'); %Ask user to confirm closing without saving
+        %Confirm closing without saving
+        choice = questdlg('You are about to close without saving.','CARMA','Close','Save','Save');
         switch choice
             case 'Close'
                 program_reset(handles); %Run PROGRAM_RESET function
@@ -156,6 +165,7 @@ function save_rating(handles)
         end
     end
     program_reset(handles); %Run PROGRAM_RESET function
+    %Report saving success or failure
     if success==1
         msgbox('Save successful.');
     else
@@ -188,11 +198,13 @@ function make_changes(Settings,handles)
         colorGradient(Settings.axis_color2,Settings.axis_color3,225,70)]);
     set(handles.slider,'Min',axis_min,'Max',axis_max);
     set(handles.slider,'Value',axis_max-(axis_max-axis_min)/2); %Set slider to midpoint value
-    axis ij; hold on; %Configure axis image
+    axis ij; hold on;
+    %Plot hash-marks on rating axis
     for i = 1:axis_steps-1
-        plot([1,5],[450,450]*i/axis_steps,'k-'); %Plot left hash-marks on axis
-        plot([65,70],[450,450]*i/axis_steps,'k-'); %Plot right hash-marks on axis
+        plot([1,5],[450,450]*i/axis_steps,'k-');
+        plot([65,70],[450,450]*i/axis_steps,'k-');
     end
+    %Plot numerical labels on rating axis
     lin = linspace(axis_max,axis_min,axis_steps);
     for i = 1:length(lin)
         text(37.5,(((450*(i-1))/axis_steps)+((450*i)/axis_steps))/2,sprintf('%.2f',lin(i)),'HorizontalAlignment','center');
