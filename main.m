4a2c5961e23903880d30792e592f22f5c644ee71
Jeffrey Girard <jmg174@pitt.edu>
2014-11-03 13:28:21 -0500

    changes for v8.02

--- main.m
+++ main.m
@@ -149,7 +149,7 @@ function figure_main_CloseReq(hObject,~)
     handles = guidata(hObject);
     % Pause playback and rating
     handles.wmp.controls.pause();
-    if isvalid(handles.timer), stop(handles.timer); end
+    if isfield(handles,'timer'), stop(handles.timer); end
     set(handles.toggle_playpause,'String','Resume','Value',0);
     % Prompt user if they really want to close
     choice = questdlg('Do you really want to close CARMA?', ...
