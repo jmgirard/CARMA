21d6560e984f6d5aeaaba6866adf88e88a249c88
Jeff <Jeff@uxEleventh.uccs.edu>
2015-10-07 08:57:28 -0600

    Updated ICC functions for more efficiency and to add the consistency ICC formulations.

--- ICC_A_k.m
+++ ICC_A_k.m
@@ -21,39 +21,26 @@ function [ICC,LB,UB] = ICC_A_k(DATA,ALPHA)
 %% Remove any missing values
 [rowindex,~] = find(~isfinite(DATA));
 DATA(rowindex,:) = [];
-%% Calculate descriptive statistics
+%% Calculate mean squares from two-way ANOVA
+[~,tbl,~] = anova2(DATA,1,'off');
+MSC = tbl{2,4};
+MSR = tbl{3,4};
+MSE = tbl{4,4};
+%% Calculate average rater agreement ICC
 [n,k] = size(DATA);
-y = mean(DATA(:));
-y_j = mean(DATA,1);
-y_i = mean(DATA,2);
-%% Calculate row, column, and error sums of squares
-SSR = 0;
-SSC = 0;
-SSE = 0;
-for i = 1:n
-    for j = 1:k
-        SSR = SSR + (y_i(i) - y)^2;
-        SSC = SSC + (y_j(j) - y)^2;
-        SSE = SSE + (DATA(i,j) - y_j(j) - y_i(i) + y)^2;
-    end
-end
-%% Calculate the mean sums of squares and ICC(A,k)
-MSR = SSR / (n - 1);
-MSE = SSE / ((n - 1)*(k - 1));
-MSC = SSC / (k - 1);
 ICC = (MSR - MSE) / (MSR + (MSC - MSE)/n);
 %% Calculate the confidence interval if requested
 if nargout > 1
     if nargin < 2
         ALPHA = 0.05;
     end
-    a   = (k*ICC) / (n*(1 - ICC));
-    b   = 1 + (k*ICC*(n - 1))/(n*(1 - ICC));
-    v   = ((a*MSC + b*MSE)^2) / (((a*MSC)^2)/(k - 1) + ((b*MSE)^2)/((n - 1)*(k - 1)));
+    c  = ICC / (n*1 - ICC);
+    d  = 1 + (p*(n - 1)) / (n*(1 - ICC));
+    v  = ((c*MSC + d*MSE)^2) / (((c*MSC)^2)/(k - 1) + ((d*MSE)^2)/((n - 1)*(k - 1)));
     FL = finv((1 - ALPHA/2),(n - 1),v);
     FU = finv((1 - ALPHA/2),v,(n - 1));
-    LB  = (n*(MSR - FL*MSE)) / (FL*(MSC - MSE) + n*MSR);
-    UB  = (n*(FU*MSR - MSE)) / (MSC - MSE + n*FU*MSR);
+    LB = (n*(MSR - FL*MSE)) / (FL*(MSC - MSE) + n*MSR);
+    UB = (n*(FU*MSR - MSE)) / (MSC - MSE + n*FU*MSR);
 end
 
 end
--- ICC_C_1.m
+++ ICC_C_1.m
@@ -1,6 +1,6 @@
-function [ICC,LB,UB] = ICC_A_1(DATA,ALPHA)
-% Calculate the single rater agreement intraclass correlation coefficient
-%   [ICC,LB,UB] = ICC_A_1(DATA)
+function [ICC,LB,UB] = ICC_C_1(DATA,ALPHA)
+% Calculate the single rater consistency intraclass correlation coefficient
+%   [ICC,LB,UB] = ICC_C_1(DATA)
 %
 %   DATA is a numerical matrix of ratings (missing values = NaN).
 %   Each row is a single item and each column is a single rater.
@@ -8,7 +8,7 @@ function [ICC,LB,UB] = ICC_A_1(DATA,ALPHA)
 %   ALPHA is the Type I error rate for the confidence interval (optional).
 %
 %   ICC is the reliability of the ratings taken from any single included
-%   rater. Reliability is gauged as agreement on an absolute scale.
+%   rater. Reliability is gauged as consistency (not absolute agreement).
 %
 %   LB and UB are the confidence interval's lower and upper bounds.
 %
@@ -23,24 +23,20 @@ function [ICC,LB,UB] = ICC_A_1(DATA,ALPHA)
 DATA(rowindex,:) = [];
 %% Calculate mean squares from two-way ANOVA
 [~,tbl,~] = anova2(DATA,1,'off');
-MSC = tbl{2,4};
 MSR = tbl{3,4};
 MSE = tbl{4,4};
-%% Calculate single rater agreement ICC
+%% Calculate single rater consistency ICC
 [n,k] = size(DATA);
-ICC = (MSR - MSE) / (MSR + MSE*(k - 1) + (k/n)*(MSC - MSE));
+ICC = (MSR - MSE) / (MSR + MSE*(k-1));
 %% Calculate the confidence interval if requested
 if nargout > 1
     if nargin < 2
         ALPHA = 0.05;
     end
-    a  = (k*ICC) / (n*(1 - ICC));
-    b  = 1 + (k*ICC*(n - 1))/(n*(1 - ICC));
-    v  = ((a*MSC + b*MSE)^2) / (((a*MSC)^2)/(k - 1) + ((b*MSE)^2)/((n - 1)*(k - 1)));
-    FL = finv((1 - ALPHA/2),(n - 1),v);
-    FU = finv((1 - ALPHA/2),v,(n - 1));
-    LB = (n*(MSR - FL*MSE)) / (FL*(k*MSC + MSE*(k*n - k - n)) + n*MSR);
-    UB = (n*(FU*MSR - MSE)) / (k*MSC + MSE*(k*n - k - n) + n*FU*MSR);
+    FL = (MSR/MSE) / finv((1-ALPHA/2),(n - 1),(n - 1)*(k - 1));
+    FU = (MSR/MSE) / finv((1-ALPHA/2),(n - 1)*(k - 1),(n - 1));
+    LB = (FL - 1) / (FL + k - 1);
+    UB = (FU - 1) / (FU + k - 1);
 end
 
 end
